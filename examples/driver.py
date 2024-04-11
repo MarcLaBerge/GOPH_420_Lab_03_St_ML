908f45e8e61f35b930e78a60328d21e1a95d8b53
Marc Laberge <marc.laberge@ucalgary.ca>
2024-04-11 12:01:12 -0600

    data added

--- examples/driver.py
+++ examples/driver.py
@@ -1,9 +1,19 @@
 import numpy as np
+import matplotlib as plt
 from lab_03.regression import multi_regress
 
 
 def main():
+    """
+    Driver script implementation of multi linear regression for earthquake magnitude data.
+    """
+    #Grabbing data from files on the repository
+    data = np.loadtxt('data/M_data.txt')
+
+    #Dependant data 
     y = ... # enter dependent variable data (or load from a file)
+
+    #Independant data 
     Z = ... # enter independent variable data (or load from a file)
     a, e, rsq = multi_regress(y, Z)
 
